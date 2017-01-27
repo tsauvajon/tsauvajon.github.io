c2e1d1ddc203b037ca1f07a566edb97088e6b582
Thomas SAUVAJON <thomas.sauvajon@gmail.com>
2017-01-27 11:59:09 +0100

    mail : formspree.io
    test

--- js/contact_me.js
+++ js/contact_me.js
@@ -9,25 +9,26 @@ $(function() {
             // Prevent spam click and default submit behaviour
             $("#btnSubmit").attr("disabled", true);
             event.preventDefault();
-            
+
             // get values from FORM
             var name = $("input#name").val();
             var email = $("input#email").val();
             var phone = $("input#phone").val();
             var message = $("textarea#message").val();
+            var text = "Name: " + name + "<br>Message : " + message + "<br>Phone :" + phone;
             var firstName = name; // For Success/Failure Message
             // Check for white space in name for Success/Fail message
             if (firstName.indexOf(' ') >= 0) {
                 firstName = name.split(' ').slice(0, -1).join(' ');
             }
             $.ajax({
-                url: "././mail/contact_me.php",
+                url: "http://formspree.io/thomas.sauvajon@gmail.com",
                 type: "POST",
                 data: {
-                    name: name,
-                    phone: phone,
+                    // name: name,
+                    // phone: phone,
                     email: email,
-                    message: message
+                    message: text
                 },
                 cache: false,
                 success: function() {
