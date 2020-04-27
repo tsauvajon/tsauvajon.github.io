0198ec9d947e73fc693304c2b9a2407261f7e417
Thomas Sauvajon <thomas@flurosat.com>
2020-04-27 20:18:36 +1000

    2020 update

--- js/google_map.js
+++ js/google_map.js
@@ -7,7 +7,7 @@ function init() {
     // var avenueSaintLazareMontpellier = new google.maps.LatLng(43.6240474, 3.8862023, 11);
     var macclesfield = new google.maps.LatLng(53.2611378, -2.1627397, 13);
     // var paris = new google.maps.LatLng(48.8588377, 2.2770203, 12);
-    
+
     var mapOptions = {
         // How zoomed in you want the map to start at (always required)
         zoom: 6,
@@ -17,7 +17,7 @@ function init() {
 
         // How you would like to style the map. 
         scrollwheel: false,
-        styles: [{"featureType":"administrative.land_parcel","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"landscape.man_made","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"poi","elementType":"labels","stylers":[{"visibility":"off"}]},{"featureType":"road","elementType":"labels","stylers":[{"visibility":"simplified"},{"lightness":20}]},{"featureType":"road.highway","elementType":"geometry","stylers":[{"hue":"#f49935"}]},{"featureType":"road.highway","elementType":"labels","stylers":[{"visibility":"simplified"}]},{"featureType":"road.arterial","elementType":"geometry","stylers":[{"hue":"#fad959"}]},{"featureType":"road.arterial","elementType":"labels","stylers":[{"visibility":"off"}]},{"featureType":"road.local","elementType":"geometry","stylers":[{"visibility":"simplified"}]},{"featureType":"road.local","elementType":"labels","stylers":[{"visibility":"simplified"}]},{"featureType":"transit","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"water","elementType":"all","stylers":[{"hue":"#a1cdfc"},{"saturation":30},{"lightness":49}]}]
+        styles: [{ "featureType": "administrative.land_parcel", "elementType": "all", "stylers": [{ "visibility": "off" }] }, { "featureType": "landscape.man_made", "elementType": "all", "stylers": [{ "visibility": "off" }] }, { "featureType": "poi", "elementType": "labels", "stylers": [{ "visibility": "off" }] }, { "featureType": "road", "elementType": "labels", "stylers": [{ "visibility": "simplified" }, { "lightness": 20 }] }, { "featureType": "road.highway", "elementType": "geometry", "stylers": [{ "hue": "#f49935" }] }, { "featureType": "road.highway", "elementType": "labels", "stylers": [{ "visibility": "simplified" }] }, { "featureType": "road.arterial", "elementType": "geometry", "stylers": [{ "hue": "#fad959" }] }, { "featureType": "road.arterial", "elementType": "labels", "stylers": [{ "visibility": "off" }] }, { "featureType": "road.local", "elementType": "geometry", "stylers": [{ "visibility": "simplified" }] }, { "featureType": "road.local", "elementType": "labels", "stylers": [{ "visibility": "simplified" }] }, { "featureType": "transit", "elementType": "all", "stylers": [{ "visibility": "off" }] }, { "featureType": "water", "elementType": "all", "stylers": [{ "hue": "#a1cdfc" }, { "saturation": 30 }, { "lightness": 49 }] }]
     };
 
     // Get the HTML DOM element that will contain your map 
@@ -27,10 +27,11 @@ function init() {
     // Create the Google Map using out element and options defined above
     var map = new google.maps.Map(mapElement, mapOptions);
     var geocoder = new google.maps.Geocoder();
-    
+
     var addresses = [
         // '31 bis avenue Saint-Lazare 34000 MONTPELLIER',
-        'Macclesfield, UK',
+        // 'Macclesfield, UK',
+        '63 Cleveland St, Darlington, NSW 2008'
     ];
 
     for (var x = 0; x < addresses.length; x++) {
@@ -44,7 +45,11 @@ function init() {
                 lat: 53.2611378,
                 lng: -2.1627397
             }
-            var p = mcsf
+            var syd = {
+                lat: -33.8883323,
+                lng: 151.1931201,
+            }
+            var p = syd
 
             console.log('google maps search results:')
             console.log(results[0])
